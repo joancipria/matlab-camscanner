988c843896f97eca54248828b27819fe818aacdb
Joan Ciprià <joancipria@gmail.com>
2020-01-26 12:02:38 +0100

    Añadido método extrema (en pruebas)

--- src/detectCorners.m
+++ src/detectCorners.m
@@ -6,72 +6,90 @@
 % una separacion minima entre si. A veces al coger las 4
 % esquinas mas lejanas no coinciden con las esquinas
 
-function corners = detectCorners(image)
+function corners = detectCorners(image, method)
 
-    % --- Detectamos todas las esquinas ---
-    corners = detectMinEigenFeatures(image);
+    % --- MÉTODO REGIONPROPS ---
+    if strcmp(method,'extrema') == 1
+        corners = regionprops(image,'Extrema');
+        assignin('base','stats',corners);
 
-    % Cogemos las 10 mas fuertes
-    cornersNum = 10;
-    corners = corners.selectStrongest(cornersNum).Location;
+        topLeft = corners.Extrema(8,:);
+        topRight = corners.Extrema(2,:);
+        botRight = corners.Extrema(4,:);
+        botLeft = corners.Extrema(6,:);
 
-    % Obtenemos el centro del objeto
-    props = regionprops(image, 'Centroid');
-    xyCentroid = vertcat(props.Centroid);
+        % Mostramos las esquinas
+        imshow(image); hold on;
+        plot(corners.Extrema(:,1), corners.Extrema(:,end), 'r*', 'LineWidth', 0.5, 'MarkerSize', 9);
+    
+    % --- MÉTODO MANUAL ---
+    else
+        % --- Detectamos todas las esquinas ---
+        corners = detectMinEigenFeatures(image);
 
-    % --- Obtenemos las 4 esquinas mas lejanas del centro ---
-    % Separamos en X e Y los corners
-    x = corners(:,1);
-    y = corners(:,end);
+        % Cogemos las 10 mas fuertes
+        cornersNum = 10;
+        corners = corners.selectStrongest(cornersNum).Location;
 
-    % Calculamos distancias de cada corner respecto al centro
-    for k = 1 : cornersNum
-        d = sqrt((xyCentroid(:,1)-x(k)).^2 + (xyCentroid(:,end)-y(k)).^2);
-        %logMsg = [' Centroid at (',num2str(xyCentroid(:,1)),' , ', num2str(xyCentroid(:,end)), ') is at ', num2str(d), ' from point #',num2str(k),' at (',num2str(x(k)),' , ',num2str(y(k)),')'];
-        %disp(logMsg)
-        [distances(k), indexOfMax(k)] = max(d);
+        % Obtenemos el centro del objeto
+        props = regionprops(image, 'Centroid');
+        xyCentroid = vertcat(props.Centroid);
+
+        % --- Obtenemos las 4 esquinas mas lejanas del centro ---
+        % Separamos en X e Y los corners
+        x = corners(:,1);
+        y = corners(:,end);
+
+        % Calculamos distancias de cada corner respecto al centro
+        for k = 1 : cornersNum
+            d = sqrt((xyCentroid(:,1)-x(k)).^2 + (xyCentroid(:,end)-y(k)).^2);
+            %logMsg = [' Centroid at (',num2str(xyCentroid(:,1)),' , ', num2str(xyCentroid(:,end)), ') is at ', num2str(d), ' from point #',num2str(k),' at (',num2str(x(k)),' , ',num2str(y(k)),')'];
+            %disp(logMsg)
+            [distances(k), indexOfMax(k)] = max(d);
+        end
+
+        % Nos quedamos con las 4 esquinas mas lejanas 
+        [M,I] = maxk(distances,4);
+        bestCorners = corners([I],:);
+
+        % Sobreescribimos corners con las 4 esquinas
+        corners = bestCorners;
+
+
+        % --- Clasificamos esquinas ---
+
+        % Primero clasificamos esquinas en LEFT y RIGHT
+
+        % RIGHT (X maxima)
+        [rightCorners, indexRightCorners] = maxk(corners(:,1),2);
+        rightCorners = corners(indexRightCorners,:);
+
+        % LEFT (X minima)
+        [leftCorners, indexLeftCorners] = mink(corners(:,1),2);
+        leftCorners = corners(indexLeftCorners,:);
+
+        % Luego combinamos con TOP y BOTTOM y sacamos la clasificacion final
+
+        % BOTTOM LEFT
+        [botLeft, indexBotLeft] = maxk(leftCorners(:,end),1);
+        botLeft = leftCorners(indexBotLeft,:);
+
+        % BOTTOM RIGHT
+        [botRight, indexBotRight] = maxk(rightCorners(:,end),1);
+        botRight = rightCorners(indexBotRight,:);
+
+        % TOP RIGHT
+        [topRight, indexTopRight] = mink(rightCorners(:,end),1);
+        topRight = rightCorners(indexTopRight,:);
+
+        % TOP LEFT
+        [topLeft, indexTopLeft] = mink(leftCorners(:,end),1);
+        topLeft = leftCorners(indexTopLeft,:);
+
+        % Mostramos las esquinas
+        imshow(image); hold on;
+        plot(corners(:,1), corners(:,end), 'r*', 'LineWidth', 0.5, 'MarkerSize', 9);
     end
-
-    % Nos quedamos con las 4 esquinas mas lejanas 
-    [M,I] = maxk(distances,4);
-    bestCorners = corners([I],:);
-
-    % Sobreescribimos corners con las 4 esquinas
-    corners = bestCorners;
-
-    % Mostramos las esquinas
-    %imshow(image); hold on;
-    %plot(corners(:,1), corners(:,end), 'r*', 'LineWidth', 0.5, 'MarkerSize', 9);
-
-    % --- Clasificamos esquinas ---
-
-    % Primero clasificamos esquinas en LEFT y RIGHT
-
-    % RIGHT (X maxima)
-    [rightCorners, indexRightCorners] = maxk(corners(:,1),2);
-    rightCorners = corners(indexRightCorners,:);
-
-    % LEFT (X minima)
-    [leftCorners, indexLeftCorners] = mink(corners(:,1),2);
-    leftCorners = corners(indexLeftCorners,:);
-
-    % Luego combinamos con TOP y BOTTOM y sacamos la clasificacion final
-
-    % BOTTOM LEFT
-    [botLeft, indexBotLeft] = maxk(leftCorners(:,end),1);
-    botLeft = leftCorners(indexBotLeft,:);
-
-    % BOTTOM RIGHT
-    [botRight, indexBotRight] = maxk(rightCorners(:,end),1);
-    botRight = rightCorners(indexBotRight,:);
-
-    % TOP RIGHT
-    [topRight, indexTopRight] = mink(rightCorners(:,end),1);
-    topRight = rightCorners(indexTopRight,:);
-
-    % TOP LEFT
-    [topLeft, indexTopLeft] = mink(leftCorners(:,end),1);
-    topLeft = leftCorners(indexTopLeft,:);
 
     corners = [topLeft; topRight; botRight; botLeft;];
 end
